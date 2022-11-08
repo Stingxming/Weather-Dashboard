45501944332cc4e26592b8425892a4c7f4c310e0
Mathew Chandler <Cascabelrock@gmail.com>
2022-11-07 16:24:49 -0800

    Parse response for current weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,5 +25,41 @@ function initPage() {
 
                todayweatherEl.classList.remove("d-none");
 
-               
-}
+                // Parse response to display current weather
+                const currentDate = new Date(response.data.dt * 1000);
+                const day = currentDate.getDate();
+                const month = currentDate.getMonth() + 1;
+                const year = currentDate.getFullYear();
+                nameEl.innerHTML = response.data.name + " (" + month + "/" + day + "/" + year + ") ";
+                let weatherPic = response.data.weather[0].icon;
+                currentPicEl.setAttribute("src", "https://openweathermap.org/img/wn/" + weatherPic + "@2x.png");
+                currentPicEl.setAttribute("alt", response.data.weather[0].description);
+                currentTempEl.innerHTML = "Temperature: " + k2f(response.data.main.temp) + " &#176F";
+                currentHumidityEl.innerHTML = "Humidity: " + response.data.main.humidity + "%";
+                currentWindEl.innerHTML = "Wind Speed: " + response.data.wind.speed + " MPH";
+
+                 // Get UV Index
+                 let lat = response.data.coord.lat;
+                 let lon = response.data.coord.lon;
+                 let UVQueryURL = "https://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey + "&cnt=1";
+                 axios.get(UVQueryURL)
+                     .then(function (response) {
+                         let UVIndex = document.createElement("span");
+                         
+                         // When UV Index is good, shows green, when ok shows yellow, when bad shows red
+                         if (response.data[0].value < 4 ) {
+                             UVIndex.setAttribute("class", "badge badge-success");
+                         }
+                         else if (response.data[0].value < 8) {
+                             UVIndex.setAttribute("class", "badge badge-warning");
+                         }
+                         else {
+                             UVIndex.setAttribute("class", "badge badge-danger");
+                         }
+                         console.log(response.data[0].value)
+                         UVIndex.innerHTML = response.data[0].value;
+                         currentUVEl.innerHTML = "UV Index: ";
+                         currentUVEl.append(UVIndex);
+                     });
+                 
+           }
